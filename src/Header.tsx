33bbe79b995f3df5f0bb8d1d95667a226d474786
michalosinski91 <mosinski@hotmail.co.uk>
2019-09-23 13:58:08 +0100

    logout functionality added

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useMeQuery } from './generated/graphql'
+import { useMeQuery, useLogoutMutation } from './generated/graphql'
+import { setAccessToken } from './accessToken'
 
 export const Header: React.FC = () => {
-    const {data, loading} = useMeQuery({ fetchPolicy: 'network-only' })
+    const {data, loading} = useMeQuery()
+    const [logout, {client}] = useLogoutMutation()
     
     let body: any = null;
 
@@ -28,6 +30,18 @@ export const Header: React.FC = () => {
             </div>
             <div>
                 <Link to='/bye'>Bye</Link>
+            </div>
+            <div>
+                {!loading && data && data.me 
+                    ? (
+                        <button onClick={async () => {
+                            await logout()
+                            setAccessToken('')
+                            await client!.resetStore();
+                        }}>Log out</button>
+                    )
+                    : null
+                }
             </div>
             <div>{body}</div>
             
