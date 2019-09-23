6bdf9fe02dbd397755718d4057a795bdab3f6fef
michalosinski91 <mosinski@hotmail.co.uk>
2019-09-23 11:35:05 +0100

    login functionality added

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useState} from 'react'
+import { RouteComponentProps } from 'react-router'
+import { useLoginMutation } from '../generated/graphql'
 
-interface Props {
 
-}
+export const Login: React.FC<RouteComponentProps> = ({ history }) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [login] = useLoginMutation()
 
-export const Login: React.FC<Props> = () => {
     return(
-        <div>
-            login page
-        </div>
+        <form onSubmit={async e => {
+            e.preventDefault()
+            const response = await login({
+                variables: {
+                    email,
+                    password
+                }
+            })
+            console.log(response)
+            history.push('/')
+        }}>
+            <div>
+                <input 
+                    value={email} 
+                    placeholder='email' 
+                    onChange={ e => {setEmail(e.target.value)}} 
+                />
+            </div>
+            <div>
+                <input 
+                    value={password} 
+                    type='password'
+                    placeholder='password' 
+                    onChange={ e => {setPassword(e.target.value)}} 
+                />
+            </div>
+            <div>
+                <button type='submit'>Login</button>
+            </div>
+        </form>
     )
 }
