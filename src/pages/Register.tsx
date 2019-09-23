ffa9e91773e3b163384af921c31f3d04bede9cf8
michalosinski91 <mosinski@hotmail.co.uk>
2019-09-23 11:28:11 +0100

    register + display users functionality

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,13 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useRegisterMutation } from '../generated/graphql'
+import { RouteComponentProps } from 'react-router-dom'
 
-interface Props {
 
-}
+export const Register: React.FC<RouteComponentProps> = ({ history }) => {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [register] = useRegisterMutation()
 
-export const Register: React.FC<Props> = () => {
     return(
-        <div>
-            register page
-        </div>
+        <form onSubmit={async e => {
+            e.preventDefault()
+            const response = await register({
+                variables: {
+                    email,
+                    password
+                }
+            })
+            console.log(response)
+            history.push('/')
+        }}>
+            <div>
+                <input 
+                    value={email} 
+                    placeholder='email' 
+                    onChange={ e => {setEmail(e.target.value)}} 
+                />
+            </div>
+            <div>
+                <input 
+                    value={password} 
+                    type='password'
+                    placeholder='password' 
+                    onChange={ e => {setPassword(e.target.value)}} 
+                />
+            </div>
+            <div>
+                <button type='submit'>Register</button>
+            </div>
+        </form>
     )
 }
